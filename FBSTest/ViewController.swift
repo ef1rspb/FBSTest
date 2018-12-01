20182c7e6e97357ede8655846bc1ea2f8e418aec
Aleksandr Malina <ef1rspb@gmail.com>
2018-12-01 12:13:00 +0300

    Init commit

---
+++ FBSTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FBSTest
+//
+//  Created by Aleksandr Malina on 12/1/18.
+//  Copyright © 2018 Aleksandr Malina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ FBSTestTests/FBSTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  FBSTestTests.swift
+//  FBSTestTests
+//
+//  Created by Aleksandr Malina on 12/1/18.
+//  Copyright © 2018 Aleksandr Malina. All rights reserved.
+//
+
+import XCTest
+@testable import FBSTest
+
+class FBSTestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
