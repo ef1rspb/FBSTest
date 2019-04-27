8c59050f338e14dd539a08920e8a2a9f446cbbd7
Sasha Malina <ef1rspb@gmail.com>
2019-04-27 13:04:21 +0300

    Refactor User details view

--- FBSTest/Controllers/User details/UserDetailsViewController.swift
+++ FBSTest/Controllers/User details/UserDetailsViewController.swift
@@ -11,118 +11,172 @@ import RxSwift
 
 final class UserDetailsViewController: UIViewController, UserDetailsView {
 
-    var onUserUpdated: ((UserViewModel) -> Void)?
-    var viewModel: UserDetailsViewModel!
+  // MARK: - Callbacks
 
-    private let disposeBag = DisposeBag()
+  var onUserUpdated: ((UserViewModel) -> Void)?
 
-    private lazy var userAvatarImageView: UIImageView = {
-        let image = UIImage.User.avatarPlaceholder
-        let imageView = UIImageView(image: image)
-        view.addSubview(imageView)
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor).isActive = true
-        return imageView
-    }()
+  // MARK: - Properties
 
-    private lazy var updateImageButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("Update image", for: .normal)
-        button.setTitleColor(.blue, for: .normal)
-        view.addSubview(button)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.topAnchor.constraint(equalTo: userAvatarImageView.bottomAnchor, constant: 20).isActive = true
-        button.centerXAnchor.constraint(equalTo: userAvatarImageView.centerXAnchor).isActive = true
-        return button
-    }()
+  var viewModel: UserDetailsViewModel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  private let disposeBag = DisposeBag()
 
-        updateImageButton.rx.tap
-            .subscribe(onNext: { [weak self] in
-                self?.showPhotoSourceAlert()
-            })
-            .disposed(by: disposeBag)
+  private lazy var avatarImageView: UIImageView = {
+    let image = UIImage.User.avatarPlaceholder
+    let imageView = UIImageView(image: image)
+    imageView.backgroundColor = .gray
+    return imageView
+  }()
 
-        viewModel.userViewModel
-            .imageDriver
-            .drive(onNext: { [weak self] in
-                self?.userAvatarImageView.image = $0
-            })
-            .disposed(by: disposeBag)
-    }
+  private lazy var updateImageButton: UIButton = {
+    let button = UIButton()
+    button.setTitle("Update image", for: .normal)
+    button.setTitleColor(.blue, for: .normal)
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    return button
+  }()
 
-        view.backgroundColor = .white
-        title = viewModel.header
-    }
+  // MARK: - View lifecycle
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    setupView()
+
+    viewModel.userViewModel
+      .imageDriver
+      .drive(onNext: { [unowned self] in
+        self.avatarImageView.image = $0
+      })
+      .disposed(by: disposeBag)
+  }
+
+  // MARK: - Setup view
+
+  private func setupView() {
+    view.backgroundColor = .white
+    title = viewModel.header
+
+    setupAvatar()
+    setupUpdateImageButton()
+  }
+
+  private func setupAvatar() {
+    view.addSubview(avatarImageView)
+    avatarImageView.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      avatarImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      avatarImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+      avatarImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor)
+    ])
+  }
+
+  private func setupUpdateImageButton() {
+    view.addSubview(updateImageButton)
+    updateImageButton.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      updateImageButton.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 20),
+      updateImageButton.centerXAnchor.constraint(equalTo: avatarImageView.centerXAnchor)
+    ])
+
+    updateImageButton.rx.tap
+      .subscribe(onNext: { [unowned self] in
+        self.showPhotoSourceAlert()
+      })
+      .disposed(by: disposeBag)
+  }
 }
 
 extension UserDetailsViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
-    func imagePickerController(_ picker: UIImagePickerController,
-                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
-        picker.dismiss(animated: true, completion: nil)
-        if let image = info[UIImagePickerController.InfoKey.originalImage]! as? UIImage {
-            userAvatarImageView.image = image
-            viewModel.updateUser(avatarImageData: image.pngData())
-            onUserUpdated?(viewModel.userViewModel)
-        }
+  func imagePickerController(_ picker: UIImagePickerController,
+                             didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+    picker.dismiss(animated: true, completion: nil)
+    if let image = info[UIImagePickerController.InfoKey.originalImage]! as? UIImage {
+      avatarImageView.image = image
+      viewModel.updateUser(avatarImageData: image.pngData())
+      onUserUpdated?(viewModel.userViewModel)
     }
+  }
 }
+
+// MARK: - Image picker
 
 extension UserDetailsViewController {
 
-    func showPhotoSourceAlert() {
-        let alert = UIAlertController(title: "Choose Image", message: nil, preferredStyle: .actionSheet)
-        alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: { [weak self] _ in
-            self?.openCamera()
-        }))
+  enum ImagePickerSourceType {
 
-        alert.addAction(UIAlertAction(title: "Gallery", style: .default, handler: { [weak self] _ in
-            self?.openGallery()
-        }))
+    case camera
+    case photoLibrary
 
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-
-        self.present(alert, animated: true, completion: nil)
+    var alertMessage: String {
+      switch self {
+      case .camera:
+        return "You don't have perission to access camera"
+      case .photoLibrary:
+        return "You don't have perission to access gallery."
+      }
     }
 
-    private func openCamera() {
-        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
-            let imagePicker = UIImagePickerController()
-            imagePicker.delegate = self
-            imagePicker.sourceType = UIImagePickerController.SourceType.camera
-            imagePicker.allowsEditing = false
-            self.present(imagePicker, animated: true, completion: nil)
-        } else {
-            let alert  = UIAlertController(title: "Warning",
-                                           message: "You don't have perission to access camera",
-                                           preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
-        }
+    var uiImagePickerControllerSourceType: UIImagePickerController.SourceType {
+      switch self {
+      case .camera:
+        return .camera
+      case .photoLibrary:
+        return .photoLibrary
+      }
     }
+  }
 
-    private func openGallery() {
-        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.photoLibrary) {
-            let imagePicker = UIImagePickerController()
-            imagePicker.delegate = self
-            imagePicker.allowsEditing = true
-            imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
-            self.present(imagePicker, animated: true, completion: nil)
-        } else {
-            let alert  = UIAlertController(title: "Warning",
-                                           message: "You don't have perission to access gallery.",
-                                           preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
-        }
+  private func showImagePicker(type: ImagePickerSourceType) {
+    let sourceType = type.uiImagePickerControllerSourceType
+    if UIImagePickerController.isSourceTypeAvailable(sourceType) {
+      let imagePicker = createImagePicker(sourceType: sourceType, delegate: self)
+      present(imagePicker, animated: true, completion: nil)
+    } else {
+      let alert = createWarningOkAlert(message: type.alertMessage)
+      present(alert, animated: true, completion: nil)
     }
+  }
+
+  private func showPhotoSourceAlert() {
+    let alert = createChoosePhotoSourceView()
+    present(alert, animated: true, completion: nil)
+  }
+
+  private func createChoosePhotoSourceView() -> UIViewController {
+    let alert = UIAlertController(title: "Choose source", message: nil, preferredStyle: .actionSheet)
+    alert.addAction(UIAlertAction(title: "Camera", style: .default) { [weak self] _ in
+      self?.showImagePicker(type: .camera)
+    })
+
+    alert.addAction(UIAlertAction(title: "Gallery", style: .default) { [weak self] _ in
+      self?.showImagePicker(type: .photoLibrary)
+    })
+
+    alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+    return alert
+  }
+
+  private func createWarningOkAlert(message: String) -> UIViewController {
+    let alert  = UIAlertController(
+      title: "Warning",
+      message: message,
+      preferredStyle: .alert
+    )
+    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+    return alert
+  }
+
+  private func createImagePicker(
+    sourceType: UIImagePickerController.SourceType,
+    delegate: (UIImagePickerControllerDelegate & UINavigationControllerDelegate)?
+    ) -> UIViewController {
+    let imagePicker = UIImagePickerController()
+    imagePicker.delegate = delegate
+    imagePicker.sourceType = sourceType
+
+    return imagePicker
+  }
 }
